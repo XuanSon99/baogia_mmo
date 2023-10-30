3db63866cf8e19ab4b5a83fed019267888a1e518
Your Name <you@example.com>
2023-10-30 16:23:16 +0700

    change

--- bot.py
+++ bot.py
@@ -54,6 +54,6 @@ async def callback_minute(context: ContextTypes.DEFAULT_TYPE):
 
 job_queue = app.job_queue
 
-job_minute = job_queue.run_repeating(callback_minute, interval=300, first=10)
+job_minute = job_queue.run_repeating(callback_minute, interval=180, first=10)
 
 app.run_polling()
