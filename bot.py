0bf3c1987c9ac33cc8e2ccd31cfc9059a3661325
XuanSon99 <64201776+XuanSon99@users.noreply.github.com>
2024-05-30 22:41:17 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ async def callback_minute(context: ContextTypes.DEFAULT_TYPE):
 
     reply_markup = InlineKeyboardMarkup(
         [[InlineKeyboardButton(text='Xem tỷ giá', url='https://moneyexchange247.com'),
-          InlineKeyboardButton(text='Mua bán PAYPAL', url='https://moneyexchange247.com')]])
+          InlineKeyboardButton(text='Mua bán ngay', url='https://moneyexchange247.com')]])
 
     buy = requests.get(
         f"{domain}/api/p2p?type=buy&asset=usdt&fiat=vnd&page=1")
